0521ace31f126b05568a97cc84a1b7da08ab2777
Anthony Thiery <anthony.thiery@vivoka.com>
2025-02-03 21:24:08 +0100

    feat : add composer

---
+++ models/users.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from orm.database import Base, DEFAULT_LENGTH, RecordTimestamps
+
+
+class User(Base, RecordTimestamps):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(DEFAULT_LENGTH), unique=True, nullable=False)
+    hashed_password = Column(String(DEFAULT_LENGTH), nullable=False)
+    is_active = Column(Boolean, default=False)
+    otp_code = Column(String(6), nullable=True)
+    otp_valid_until = Column(DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"<User(email={self.email}, is_active={self.is_active})>"

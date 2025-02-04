9ec5a59002791c3959adba6532eee6c328188227
Anthony Thiery <anthony.thiery@vivoka.com>
2025-02-04 09:16:04 +0100

    fix : mixins

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Kipito-orm",
+    name="orm-package",  # Le nom que vous utiliserez pour pip install
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["SQLAlchemy", "psycopg2-binary"],
+    install_requires=[
+        "sqlalchemy>=1.4.0",
+        # Ajoutez ici vos autres dépendances
+    ],
+    python_requires=">=3.7",
+    author="Votre Nom",
+    author_email="votre@email.com",
+    description="Une description courte de votre package",
 )

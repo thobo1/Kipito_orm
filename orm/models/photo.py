d312e123c3fe162e1fe063004727691db26b7de4
Anthony Thiery <anthony.thiery@vivoka.com>
2025-02-21 12:08:16 +0100

    fix

--- orm/models/photo.py
+++ orm/models/photo.py
@@ -10,12 +10,11 @@ class Photo(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"))
-    photo_url = Column(String, nullable=False)
     name_on_s3 = Column(UUID(as_uuid=True), default=uuid.uuid4, nullable=False)
 
     # Relation
     post = relationship("Post", back_populates="photos")
-    
+
     def __init__(self, post_id, photo_url, name_on_s3=None):
         self.post_id = post_id
         self.photo_url = photo_url
@@ -23,4 +22,3 @@ class Photo(Base):
             self.name_on_s3 = name_on_s3
         else:
             self.name_on_s3 = uuid.uuid4()
-

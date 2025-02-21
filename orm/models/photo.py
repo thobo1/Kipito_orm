481e42641761b2f2dcd4c01ebbcfa9625125b5ea
Anthony Thiery <anthony.thiery@vivoka.com>
2025-02-21 10:01:45 +0100

    feat : add name on s3 for photo

--- orm/models/photo.py
+++ orm/models/photo.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from orm.database import Base
+import uuid
 
 
 class Photo(Base):
@@ -9,6 +11,7 @@ class Photo(Base):
     id = Column(Integer, primary_key=True, index=True)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"))
     photo_url = Column(String, nullable=False)
+    name_on_s3 = Column(UUID(as_uuid=True), default=uuid.uuid4, nullable=False)
 
     # Relation
     post = relationship("Post", back_populates="photos")
